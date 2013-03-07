c9e613832289a12320b7bf9fd99e3ed8abf7b012
Matt McClure <matthewlmcclure@gmail.com>
2013-03-07 10:19:01 -0500

    Works with Emacs for Mac OS X.
    Rather than terminal emacs that ships with Mac OS.
    
    Committing changes from 2012-10-24.

--- emacs-git-difftool.sh
+++ emacs-git-difftool.sh
@@ -1,7 +1,7 @@
-#! /bin/bash -i
+#! /bin/bash
 
 LOCAL="$1"
 REMOTE="$2"
 
 rootdir="$(dirname "$BASH_SOURCE")"
-emacs --eval="(progn (setq debug-on-error t) (load-file \"$rootdir/dircmp.el\") (compare-directories \"$LOCAL\" \"$REMOTE\"))"
+emacs --eval "(progn (setq debug-on-error t) (load-file \"$rootdir/dircmp.el\") (compare-directories \"$LOCAL\" \"$REMOTE\"))"
